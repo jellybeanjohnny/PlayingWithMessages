6e7a770f6048a044889c307ddefebea03ab42601
jellybeanjohnny <mamerige@icloud.com>
2016-08-31 11:35:46 -0400

    Adds logic to switch between the drawings and canvas view controller depending on the presentation style

--- MessagesExtension/CanvasViewController.swift
+++ MessagesExtension/CanvasViewController.swift
@@ -10,6 +10,6 @@ import UIKit
 
 class CanvasViewController: UIViewController {
 
-
+  static let storyboardIdentifier = "CanvasViewController"
 
 }
---
+++ MessagesExtension/DrawingsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DrawingsViewController.swift
+//  PlayingWithMessages
+//
+//  Created by Matt Amerige on 8/31/16.
+//  Copyright © 2016 mogumogu. All rights reserved.
+//
+
+import UIKit
+
+class DrawingsViewController: UIViewController {
+  
+  // MARK: Properties
+  static let storyboardIdentifier = "DrawingsViewController"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
