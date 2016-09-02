1819148a5674e0739ce13063874c641da4e060b6
jellybeanjohnny <mamerige@icloud.com>
2016-09-02 10:56:19 -0400

    Adds save and fetch method for working with cloudkit

--- MessagesExtension/CloudKitInterface.swift
+++ MessagesExtension/CloudKitInterface.swift
@@ -11,14 +11,39 @@ import CloudKit
 
 class CloudKitInterface {
   
-  let publicDatabase = CKContainer.default().publicCloudDatabase
+  private static let publicDatabase = CKContainer.default().publicCloudDatabase
   
-  class func save(drawing: UIImage) {
+  class func save(drawing: UIImage, completion:@escaping (String?, Error?) -> ()) {
     
+    let newRecord = CKRecord(recordType: "image")
+    do {
+      newRecord["fullsize"] = try CKAsset(image: drawing)
+      
+      CloudKitInterface.publicDatabase.save(newRecord, completionHandler: { (record, error) in
+        if let recordID = record?.recordID.recordName {
+          completion(recordID, nil)
+        } else {
+          completion(nil, error)
+        }
+      })
+    } catch {
+      completion(nil, error)
+    }
   }
   
-  class func load() {
-    
+  class func fetchImage(withIdentifier imageIdentifier: String, completion:@escaping (UIImage?, Error?) -> ()) {
+    let imageRecordID = CKRecordID(recordName: imageIdentifier)
+    let fetchOperation = CKFetchRecordsOperation(recordIDs: [imageRecordID])
+    fetchOperation.desiredKeys = ["fullsize"]
+    fetchOperation.fetchRecordsCompletionBlock = { (records, error) in
+      if let imageRecord = records?[imageRecordID], let asset = imageRecord["fullsize"] as? CKAsset, let image = asset.image {
+        completion(image, nil)
+      } else {
+        completion(nil, error)
+      }
+    }
+    CloudKitInterface.publicDatabase.add(fetchOperation)
   }
+  
   
 }
